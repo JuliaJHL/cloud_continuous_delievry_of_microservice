ee3d35285790742a42a16b32ad69c6430656ef30
Julia_Jin <hj146@duke.edu>
2023-01-23 15:39:59 -0500

    create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """return an HTTP greeting and login interface."""
+    return render_template("login.html")
+
+
+@app.route('/login', methods=["post"])
+def login():
+    user_name = request.form.get("user_name")
+    user_pwd = request.form.get("user_pwd")
+    if user_name == "IDS721" and user_pwd == "DUKE2023":
+        return "Login successful!"
+    else:
+        return "Login failed!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
